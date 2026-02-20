2518c92ac8effab4d73a0d30136811825c292193
Admin <Admin@DESKTOP-B6M267U>
2026-02-20 11:59:42 +0200

    adding update attempt 1

---
+++ PD411_Shop/ViewModels/PaginationVM.cs
@@ -0,0 +1,9 @@
+﻿namespace PD411_Shop.ViewModels
+{
+    public class PaginationVM
+    {
+        public int Page { get; set; } = 1;
+        public int PageCount { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
---
+++ PD411_Shop/ViewModels/UpdateProductVM.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace PD411_Shop.ViewModels
+{
+    public class UpdateProductVM
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Обов'язкове поле")]
+        [MaxLength(200, ErrorMessage = "Максимальна довжина 200 символів")]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "Потрібно вказати ціну")]
+        [Range(0, 99999999, ErrorMessage = "К-сть повинна бути в діапазоні 0 - 99999999")]
+        public double Price { get; set; } = 0d;
+        [Required(ErrorMessage = "Потрібно вказати кількість")]
+        [Range(0, 10000, ErrorMessage = "К-сть повинна бути в діапазоні 0 - 10000")]
+        public int Amount { get; set; } = 0;
+        [Required(ErrorMessage = "Обов'язкове поле")]
+        [MaxLength(100, ErrorMessage = "Максимальна довжина 100 символів")]
+        public string? Color { get; set; }
+        public IFormFile? Image { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<SelectListItem> SelectCategories { get; set; } = [];
+    }
+}
